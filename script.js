9b35d29d7302b2329a6b59c1141f2192583fbe6b
Victor <victor.hugofla@hotmail.com>
2021-09-14 16:09:38 -0300

    Finaliza requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const balls = document.getElementsByClassName('ball');
+const colorToGuess = document.getElementById('rgb-color');
+const answerText = document.getElementById('answer');
 
 function randomColor() {
   const r = parseInt(Math.random() * 255, 10);
@@ -13,6 +15,31 @@ function addBallsColor() {
   }
 }
 addBallsColor();
+
+function sortColor() {
+  const randomIndex = parseInt(Math.random() * 6, 10);
+  const getStyle = window.getComputedStyle(balls[randomIndex]).getPropertyValue('background-color');
+  colorToGuess.innerText = getStyle;
+}
+sortColor();
+
+function checkCorrectBall(e) {
+  const bgColor = 'background-color';
+  const checkClickedElement = window.getComputedStyle(e.target).getPropertyValue(bgColor);
+  const checkRightColor = colorToGuess.innerText;
+  if (checkClickedElement === checkRightColor) {
+    answerText.innerText = 'Acertou!';
+  } else if (checkClickedElement !== checkRightColor) {
+    answerText.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+function addEventToBalls() {
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].addEventListener('click', checkCorrectBall);
+  }
+}
+addEventToBalls();
 
 /* Referências:
 Gerar cor aleatória:
