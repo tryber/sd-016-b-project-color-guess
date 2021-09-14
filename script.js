58c336405eb8a13421a1239c00762e797f1eb1f2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-14 15:27:16 -0300

    creation of getAnswer function

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const user = {
   randomColor: [],
+  starting: true,
   hit: false,
   points: 0,
 }
+
+const aswer = getOne('#answer');
 
 function randomizeColors() {
   return Math.floor(Math.random() * 255);
@@ -26,5 +29,21 @@ function coloringBalls() {
   });
 }
 
+function getAswer() {
+  const possibleAnswers = ['Escolha uma cor', 'Acertou', 'Errou! Tente novamente'];
+
+  if (user.starting) {
+    aswer.innerText = possibleAnswers[0];
+    user.starting = false;
+  }
+}
+
+function startingGame() {
+  generateRandomColors();
+  coloringBalls();
+  getAswer();
+}
+
 window.onload = () => {
+  // startingGame();
 }
