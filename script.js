3ace02265d96e85526494d4c32c82836241714fc
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-14 17:14:04 -0300

    Requisito 07 - Completo

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const colorCircle = document.getElementsByClassName('ball')
-const colorCircleContainer = document.getElementById('colors-option-container')
-const resetGameButton = document.getElementById('reset-game')
-const rgbQuestion = document.getElementById('rgb-color')
-const answerParagraph = document.getElementById('answer')
+const colorCircle = document.getElementsByClassName('ball');
+const colorCircleContainer = document.getElementById('colors-option-container');
+const resetGameButton = document.getElementById('reset-game');
+const rgbQuestion = document.getElementById('rgb-color');
+const answerParagraph = document.getElementById('answer');
+const score = document.getElementById('score');
+let contador = 0
 
-resetGameButton.addEventListener('click', resetGame)
-colorCircleContainer.addEventListener('click', compareteAnswer)
+resetGameButton.addEventListener('click', resetGame);
+colorCircleContainer.addEventListener('click', compareteAnswer);
+answerParagraph.addEventListener('change', resetGame )
+
 
 function resetText() {
   answerParagraph.innerHTML = 'Escolha uma cor'
 }
 
 function randomRGB() {
-  let numero = Math.ceil(Math.random() * 255)
-  return numero
+  let numero = Math.ceil(Math.random() * 255);
+  return numero;
 }
 
 function generateColors() {
@@ -26,41 +30,43 @@ function generateColors() {
 }
 
 function generateAnswer() {
-  let = rgbArray = []
+  let = rgbArray = [];
   for (let i = 0; i < colorCircle.length; i += 1) {
-    let rgb = window.getComputedStyle(colorCircle[i]).backgroundColor
-    let rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',')
-    rgbArray.push(rgbSlipted)
+    let rgb = window.getComputedStyle(colorCircle[i]).backgroundColor;
+    let rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',');
+    rgbArray.push(rgbSlipted);
   }
-  let answer = []
-  answer = rgbArray[Math.ceil(Math.random()* rgbArray.length - 1)]
-  let r = answer[0]
-  let g = answer[1]
-  let b = answer[2]
-  rgbQuestion.innerHTML = `(${r}, ${g}, ${b})`
+  let answer = [];
+  answer = rgbArray[Math.ceil(Math.random()* rgbArray.length - 1)];;
+  let r = answer[0];
+  let g = answer[1];
+  let b = answer[2];
+  rgbQuestion.innerHTML = `(${r}, ${g}, ${b})`;
 }
 
 function compareteAnswer(event) {
-  const eventTarget = event.target
-  let question = rgbQuestion.innerHTML
-  let rgb = window.getComputedStyle(eventTarget).backgroundColor
-  let rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',')
-  let rgbCircle = `(${rgbSlipted[0]}, ${rgbSlipted[1]}, ${rgbSlipted[2]})`
-  let contador = 0
+  const eventTarget = event.target;
+  let question = rgbQuestion.innerHTML;
+  let rgb = window.getComputedStyle(eventTarget).backgroundColor;
+  let rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',');
+  let rgbCircle = `(${rgbSlipted[0]}, ${rgbSlipted[1]}, ${rgbSlipted[2]})`;
   if(rgbCircle === question) {
-   answerParagraph.innerHTML = 'Acertou!'
+    contador += 3
+   answerParagraph.innerHTML = 'Acertou!';
+   score.innerText = `Placar: ${contador}`
   } else {
-    answerParagraph.innerHTML = 'Errou! Tente novamente!!'
+    answerParagraph.innerHTML = 'Errou! Tente novamente!!';
   }
 }
 
 function resetGame() {
-  resetText()
-  generateColors()
-  generateAnswer()
+  resetText();
+  generateColors();
+  generateAnswer();
+  
 }
 
 window.onload = () => {
-  generateColors()
-  generateAnswer()
+  generateColors();
+  generateAnswer();
 }
