9fcf90c16921efadfe9f0fa79d329de885639b2b
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-14 15:08:22 -0300

    fixing user obj and criation of random colors functions

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-user = {
+const user = {
   randomColor: '',
   hit: false,
   points: 0,
+}
+
+function randomizeColors() {
+  return Math.floor(Math.random() * 255);
+}
+
+function generateRandomColors() {
+  for (let i = 0; i < 6; i += 1) {
+    user.randomColor += `rgb(${randomizeColors()}, ${randomizeColors()}, ${randomizeColors()})`
+  }
 }
