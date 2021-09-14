a0a0bca708a27538b03af28563229b59f1f5a1cf
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-14 19:09:32 -0300

    corrige cor a ser adivinhada

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ for (let index = 0; index < balls.length; index += 1) {
   let num2 = Math.round(Math.random() * 255);
   let num3 = Math.round(Math.random() * 255);
 
-  balls[index].style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`
+  balls[index].style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`;
 }
+
+let rgbColor = document.getElementById('rgb-color');
+let ballIndex = Math.round(Math.random() * 5);
+let colorToGuess = window.getComputedStyle(balls[ballIndex]).getPropertyValue('background-color');
+
+rgbColor.innerText = colorToGuess;
