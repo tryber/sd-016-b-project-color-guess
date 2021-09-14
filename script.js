9fd9d024ef7a4174a39e9074924ed210d6c1f2e1
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-09-14 17:52:16 -0300

    Conclui requisito 3

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const randomColor = document.getElementById('rgb-color');
-randomColor.innerHTML = '('
-let numbers = [];
+const main = document.getElementById('main-screen')
+randomColor.innerHTML = '(';
+const numbers = [];
 for (let i = 0; i < 3; i += 1) {
   numbers.push(Math.round(Math.random() * 255));
   if (i === 2) {
-    randomColor.innerHTML += `${numbers[i]}`
+    randomColor.innerHTML += `${numbers[i]}`;
   } else {
-    randomColor.innerHTML += `${numbers[i]}, `
+    randomColor.innerHTML += `${numbers[i]}, `;
     console.log();
   }
 }
-randomColor.innerHTML += ')'
+randomColor.innerHTML += ')';
+
+// Criando as divs com as cores
+for (let i = 0; i < 6; i += 1) {
+  const cor = document.createElement('div');
+  cor.classList.add('ball')
+  main.appendChild(cor);
+}
 
