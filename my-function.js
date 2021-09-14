e65dca1cd65e73d81378a355cb8ef1d361b7c6c6
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-14 14:51:08 -0300

    implement my generic functions

---
+++ my-function.js
@@ -0,0 +1,59 @@
+function getOne(element) {
+  return document.querySelector(element);
+}
+
+function getAll(element) {
+  return document.querySelectorAll(element);
+}
+
+function createElement(tag) {
+  return document.createElement(tag);
+}
+
+function addClass(element, newClass) {
+  element.classList.add(newClass);
+}
+
+function toggleClass(element, alternateClass) {
+  element.classList.toggle(alternateClass);
+}
+
+function removeClass(element, delClass) {
+  element.classList.remove(delClass);
+}
+
+function plugHtml(fatherElement, sonElement) {
+  fatherElement.appendChild(sonElement);
+}
+
+function removeOfHtml(fatherElement, sonElement) {
+  if (fatherElement.removeChild(sonElement)) {
+    fatherElement.removeChild(sonElement);
+  } else {
+    sonElement.remove();
+  }
+}
+
+function addMultiplesEvents(element, eventsName, listener) {
+  const events = eventsName.split(' ');
+
+  events.forEach((event) => {
+    element.addEventListener(event, listener, false);
+  });
+}
+
+function addMultiplesListeners(arr, eventName, listener) {
+  arr.forEach((element) => {
+    element.addEventListener(eventName, listener, false);
+  });
+}
+
+function addMultiplesEventsAndListeners(arr, eventsName, listener) {
+  const events = eventsName.split(' ');
+
+  arr.forEach((element) => {
+    events.forEach((event) => {
+      element.addEventListener(event, listener, false);
+    });
+  });
+}
