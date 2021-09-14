723e0be8c909ad22021eed4da77d08e5eaa982d1
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-14 17:24:53 -0300

    Lint JS

--- script.js
+++ script.js
@@ -4,54 +4,49 @@ const resetGameButton = document.getElementById('reset-game');
 const rgbQuestion = document.getElementById('rgb-color');
 const answerParagraph = document.getElementById('answer');
 const score = document.getElementById('score');
-let contador = 0
-
-resetGameButton.addEventListener('click', resetGame);
-colorCircleContainer.addEventListener('click', compareteAnswer);
-answerParagraph.addEventListener('change', resetGame )
-
+let contador = 0;
 
 function resetText() {
-  answerParagraph.innerHTML = 'Escolha uma cor'
+  answerParagraph.innerHTML = 'Escolha uma cor';
 }
 
 function randomRGB() {
-  let numero = Math.ceil(Math.random() * 255);
+  const numero = Math.ceil(Math.random() * 255);
   return numero;
 }
 
 function generateColors() {
   for (let i = 0; i < colorCircle.length; i += 1) {
-    let red = randomRGB();
-    let green = randomRGB();
-    let blue = randomRGB();
+    const red = randomRGB();
+    const green = randomRGB();
+    const blue = randomRGB();
     colorCircle[i].style.backgroundColor = `rgb(${red},${green},${blue} )`;
   }
 }
 
 function generateAnswer() {
-  let = rgbArray = [];
+  const rgbArray = [];
   for (let i = 0; i < colorCircle.length; i += 1) {
-    let rgb = window.getComputedStyle(colorCircle[i]).backgroundColor;
-    let rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',');
+    const rgb = window.getComputedStyle(colorCircle[i]).backgroundColor;
+    const rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',');
     rgbArray.push(rgbSlipted);
   }
   let answer = [];
-  answer = rgbArray[Math.ceil(Math.random()* rgbArray.length - 1)];;
-  let r = answer[0];
-  let g = answer[1];
-  let b = answer[2];
+  answer = rgbArray[Math.ceil(Math.random() * rgbArray.length - 1)];
+  const r = answer[0];
+  const g = answer[1];
+  const b = answer[2];
   rgbQuestion.innerHTML = `(${r}, ${g}, ${b})`;
 }
 
 function compareteAnswer(event) {
   const eventTarget = event.target;
-  let question = rgbQuestion.innerHTML;
-  let rgb = window.getComputedStyle(eventTarget).backgroundColor;
-  let rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',');
-  let rgbCircle = `(${rgbSlipted[0]}, ${rgbSlipted[1]}, ${rgbSlipted[2]})`;
-  if(rgbCircle === question) {
-    contador += 3
+  const question = rgbQuestion.innerHTML;
+  const rgb = window.getComputedStyle(eventTarget).backgroundColor;
+  const rgbSlipted = rgb.replace(/[^\d,]/g, '').split(',');
+  const rgbCircle = `(${rgbSlipted[0]}, ${rgbSlipted[1]}, ${rgbSlipted[2]})`;
+  if (rgbCircle === question) {
+    contador += 3;
     answerParagraph.innerHTML = 'Acertou!';
     score.innerText = `Placar: ${contador}`;
     generateColors();
@@ -67,10 +62,11 @@ function resetGame() {
   resetText();
   generateColors();
   generateAnswer();
-  
 }
 
 window.onload = () => {
   generateColors();
   generateAnswer();
-}
+  resetGameButton.addEventListener('click', resetGame);
+  colorCircleContainer.addEventListener('click', compareteAnswer);
+};
