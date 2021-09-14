63e7e76f81e74ab9b3c7e10901b4539bc1e70fdb
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-14 15:19:11 -0300

    cretion of functions to color the divs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const user = {
-  randomColor: '',
+  randomColor: [],
   hit: false,
   points: 0,
 }
@@ -10,6 +10,21 @@ function randomizeColors() {
 
 function generateRandomColors() {
   for (let i = 0; i < 6; i += 1) {
-    user.randomColor += `rgb(${randomizeColors()}, ${randomizeColors()}, ${randomizeColors()})`
+    user.randomColor.push(`rgb(${randomizeColors()}, ${randomizeColors()}, ${randomizeColors()})`);
   }
+}
+
+function getAllBalls() {
+  return getAll('.ball');
+}
+
+function coloringBalls() {
+  const balls = getAllBalls();
+
+  balls.forEach((ball, i) => {
+    ball.style.backgroundColor = user.randomColor[i];
+  });
+}
+
+window.onload = () => {
 }
