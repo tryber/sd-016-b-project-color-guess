fecc8c0f0d4191c934c3eadf3660e97f358f532a
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-15 16:11:50 -0300

    requisito 6 concluido

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 let balls = document.getElementsByClassName('ball');
-
-for (let index = 0; index < balls.length; index += 1) {
-  let num1 = Math.round(Math.random() * 255);
-  let num2 = Math.round(Math.random() * 255);
-  let num3 = Math.round(Math.random() * 255);
-
-  balls[index].style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`;
-}
-
 let rgbColor = document.getElementById('rgb-color');
 let ballIndex = Math.round(Math.random() * 5);
-let colorToGuess = window.getComputedStyle(balls[ballIndex]).getPropertyValue('background-color');
-
-rgbColor.innerText = colorToGuess;
-
 let answer = document.getElementById('answer');
+
+function setColors() {
+  for (let index = 0; index < balls.length; index += 1) {
+    let num1 = Math.round(Math.random() * 255);
+    let num2 = Math.round(Math.random() * 255);
+    let num3 = Math.round(Math.random() * 255);
+
+    balls[index].style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`;
+  }
+
+  let colorToGuess = window.getComputedStyle(balls[ballIndex]).getPropertyValue('background-color');
+  rgbColor.innerText = colorToGuess;
+
+  answer.innerText = 'Escolha uma cor';
+
+  return answer;
+}
+
+window.onload = setColors();
 
 function guessColor(event) {
   let ballColor = window.getComputedStyle(event.target).getPropertyValue('background-color');
@@ -29,3 +35,5 @@ function guessColor(event) {
 for (let index = 0; index < balls.length; index += 1) {
   balls[index].addEventListener('click', guessColor);
 }
+
+document.getElementById('reset-game').addEventListener('click', setColors)
