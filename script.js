e58450462d88c933f0945620dace86d8c102b392
leandro <leandrochagask@gmail.com>
2021-09-17 09:43:59 -0300

    Adiciona funcoes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const rgbColor = document.querySelector('#rgb-color');
 const ballColorsContainer = document.querySelector('#ball-colors-container');
 const resetGame = document.querySelector('#reset-game');
-let score = document.querySelector('#score');
-let answer = document.querySelector('#answer');
+const score = document.querySelector('#score');
+const answer = document.querySelector('#answer');
 
-const a = parseInt(Math.random() * 255, 10);
-const b = parseInt(Math.random() * 255, 10);
-const c = parseInt(Math.random() * 255, 10);
-const rgbColorText = `(${a}, ${b}, ${c})`;
+function rgbColorTextFunction() {
+  const a = parseInt(Math.random() * 255, 10);
+  const b = parseInt(Math.random() * 255, 10);
+  const c = parseInt(Math.random() * 255, 10);
+  const rgbColorText = `(${a}, ${b}, ${c})`; 
+  rgbColor.innerText = rgbColorText;
+  return rgbColorText;
+}
 
-rgbColor.innerText = rgbColorText;
-
-function play() {
+function sortColor() { 
   score.innerText = 'VQV!';
   answer.innerText = 'Escolha uma cor';
   for (let i = 0; i < ballColorsContainer.children.length; i += 1) {
@@ -20,27 +22,34 @@ function play() {
     const f = parseInt(Math.random() * 255, 10);
     ballColorsContainer.children[i].style.backgroundColor = `rgb(${d}, ${e}, ${f})`;
   }
-  let x = parseInt(Math.random() * 6, 10);
-  if (x === 6) { x = 5 };
-  ballColorsContainer.children[x].style.backgroundColor = `rgb${rgbColorText}`;
 }
-  
-let count = 0;
-ballColorsContainer.addEventListener('click', (event) => {
-  let yourChoice = event.target;
-  if (yourChoice.style.backgroundColor === `rgb${rgbColorText}`) {
-    console.log('AeeeeÊ');
-    answer.innerText = 'Acertou!';
-    count += 3;
-    console.log(count);
-    score.innerText = `${count} pontos`;
-  } else {
-    console.log('Oh no!!!');
-    answer.innerText = 'Errou! Tente novamente!';
-    score.innerText = 'Quase !!!';
-    count = 0;
-  }
-})
 
-window.onload = play;
+let count = document.querySelector('#span-score');
+
+function play() {
+  sortColor();
+  let selectedColor = rgbColorTextFunction();
+  let x = parseInt(Math.random() * 6, 10);
+  if (x === 6) { x = 5 }
+  ballColorsContainer.children[x].style.backgroundColor = `rgb${selectedColor}`;
+  console.log(x);
+}
+
+function choice() {
+  ballColorsContainer.addEventListener('click', (event) => {
+    const yourChoice = event.target;
+    if (yourChoice.style.backgroundColor === `rgb${rgbColor.innerText}`) {
+      answer.innerText = 'Acertou!';
+      count.innerText = +count.innerText + 3;
+      console.log(count);
+      score.innerText = `${count} pontos`;
+    } else {
+      answer.innerText = 'Errou! Tente novamente!';
+      score.innerText = 'Quase !!!';
+      count.innerText = 0;
+    }
+  });
+}
+
+window.onload = () => {play(); choice()};
 resetGame.addEventListener('click', play);
