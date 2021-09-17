a96d46cd214cfc691d2b307289736af035e93bea
leandro <leandrochagask@gmail.com>
2021-09-16 21:14:18 -0300

    Adiciona requisito 02

---
+++ script.js
@@ -0,0 +1,13 @@
+let rgbColor = document.querySelector('#rgb-color')
+
+const a = parseInt(Math.random() * 255);
+const b = parseInt(Math.random() * 255);
+const c = parseInt(Math.random() * 255);
+
+// color[i].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+
+let rgbColorText = `rgb(${a}, ${b}, ${c})`;
+
+rgbColor.innerText = rgbColorText;
+
+console.log(rgbColorText);
