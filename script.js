c306f0265a82c022da472483b147b958b43ec630
leandro <leandrochagask@gmail.com>
2021-09-16 23:01:49 -0300

    Adiciona botão reiniciar

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 const rgbColor = document.querySelector('#rgb-color');
 
-const a = parseInt(Math.random() * 255, 10);
-const b = parseInt(Math.random() * 255, 10);
-const c = parseInt(Math.random() * 255, 10);
+const answerButton = document.querySelector('#answer');
 
-// color[i].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
-
-const rgbColorText = `(${a}, ${b}, ${c})`;
-
-rgbColor.innerText = rgbColorText;
-
-console.log(rgbColorText);
-
-let ballColorsContainer = document.querySelector('#ball-colors-container');
-
-console.log(ballColorsContainer.children.length);
-
-for (let i = 0; i < ballColorsContainer.children.length; i++) {
-  
+function play(){
   const a = parseInt(Math.random() * 255, 10);
   const b = parseInt(Math.random() * 255, 10);
   const c = parseInt(Math.random() * 255, 10);
+  
+  const rgbColorText = `(${a}, ${b}, ${c})`;
+  
+  rgbColor.innerText = rgbColorText;
+  
+  let ballColorsContainer = document.querySelector('#ball-colors-container');
+  
+  for (let i = 0; i < ballColorsContainer.children.length; i++) {
+    const a = parseInt(Math.random() * 255, 10);
+    const b = parseInt(Math.random() * 255, 10);
+    const c = parseInt(Math.random() * 255, 10);
+    
+    ballColorsContainer.children[i].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;  
+  }
 
-  ballColorsContainer.children[i].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;  
-}
+  const x = parseInt(Math.random() * 6, 10);
+  if (x === 6) {x = 5}
+  ballColorsContainer.children[x].style.backgroundColor = `rgb${rgbColorText}`;
+  
+  console.log(ballColorsContainer.children[x].style.backgroundColor);
+  console.log(x);
+  
+};
 
+window.onload = play;
+answerButton.addEventListener('click', play);
