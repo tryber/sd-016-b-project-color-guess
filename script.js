0a98a0c54a6ac265048cf8529ce828f5ab179211
leandro <leandrochagask@gmail.com>
2021-09-16 21:55:57 -0300

    Adiciona requisito 04

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-let rgbColor = document.querySelector('#rgb-color')
+const rgbColor = document.querySelector('#rgb-color');
 
-const a = parseInt(Math.random() * 255);
-const b = parseInt(Math.random() * 255);
-const c = parseInt(Math.random() * 255);
+const a = parseInt(Math.random() * 255, 10);
+const b = parseInt(Math.random() * 255, 10);
+const c = parseInt(Math.random() * 255, 10);
 
 // color[i].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
 
-let rgbColorText = `(${a}, ${b}, ${c})`;
+const rgbColorText = `(${a}, ${b}, ${c})`;
 
 rgbColor.innerText = rgbColorText;
 
 console.log(rgbColorText);
+
+let ballColorsContainer = document.querySelector('#ball-colors-container');
+
+console.log(ballColorsContainer.children.length);
+
+for (let i = 0; i < ballColorsContainer.children.length; i++) {
+  
+  const a = parseInt(Math.random() * 255, 10);
+  const b = parseInt(Math.random() * 255, 10);
+  const c = parseInt(Math.random() * 255, 10);
+
+  ballColorsContainer.children[i].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;  
+}
+
