3e396def8f01b6b467cce747d4f9104ea3104a80
leandro <leandrochagask@gmail.com>
2021-09-17 00:14:26 -0300

    Adiciona placar

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const rgbColor = document.querySelector('#rgb-color');
 const ballColorsContainer = document.querySelector('#ball-colors-container');
-const answerButton = document.querySelector('#answer-button');
+const resetGame = document.querySelector('#reset-game');
+let score = document.querySelector('#score');
+let answer = document.querySelector('#answer');
 
 const a = parseInt(Math.random() * 255, 10);
 const b = parseInt(Math.random() * 255, 10);
@@ -10,34 +12,35 @@ const rgbColorText = `(${a}, ${b}, ${c})`;
 rgbColor.innerText = rgbColorText;
 
 function play() {
+  score.innerText = 'VQV!';
+  answer.innerText = 'Escolha uma cor';
   for (let i = 0; i < ballColorsContainer.children.length; i += 1) {
     const d = parseInt(Math.random() * 255, 10);
     const e = parseInt(Math.random() * 255, 10);
     const f = parseInt(Math.random() * 255, 10);
     ballColorsContainer.children[i].style.backgroundColor = `rgb(${d}, ${e}, ${f})`;
   }
-  
   let x = parseInt(Math.random() * 6, 10);
   if (x === 6) { x = 5 };
   ballColorsContainer.children[x].style.backgroundColor = `rgb${rgbColorText}`;
-  
-  console.log(ballColorsContainer.children[x].style.backgroundColor);
-  console.log(x);
 }
   
-let answer = document.querySelector('#answer');
-  
+let count = 0;
 ballColorsContainer.addEventListener('click', (event) => {
   let yourChoice = event.target;
-
   if (yourChoice.style.backgroundColor === `rgb${rgbColorText}`) {
     console.log('AeeeeÊ');
     answer.innerText = 'Acertou!';
+    count += 3;
+    console.log(count);
+    score.innerText = `${count} pontos`;
   } else {
     console.log('Oh no!!!');
     answer.innerText = 'Errou! Tente novamente!';
+    score.innerText = 'Quase !!!';
+    count = 0;
   }
 })
 
 window.onload = play;
-answerButton.addEventListener('click', play);
+resetGame.addEventListener('click', play);
