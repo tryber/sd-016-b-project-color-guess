96b3e24422d3108e54124ccaa75dac2313be748d
leandro <leandrochagask@gmail.com>
2021-09-17 10:08:26 -0300

    Muda ids

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const rgbColor = document.querySelector('#rgb-color');
 const ballColorsContainer = document.querySelector('#ball-colors-container');
 const resetGame = document.querySelector('#reset-game');
-const score = document.querySelector('#score');
+const score = document.querySelector('#score-up');
 const answer = document.querySelector('#answer');
 
 function rgbColorTextFunction() {
   const a = parseInt(Math.random() * 255, 10);
   const b = parseInt(Math.random() * 255, 10);
   const c = parseInt(Math.random() * 255, 10);
-  const rgbColorText = `(${a}, ${b}, ${c})`; 
+  const rgbColorText = `(${a}, ${b}, ${c})`;
   rgbColor.innerText = rgbColorText;
   return rgbColorText;
 }
 
-function sortColor() { 
+function sortColor() {
   score.innerText = 'VQV!';
   answer.innerText = 'Escolha uma cor';
   for (let i = 0; i < ballColorsContainer.children.length; i += 1) {
@@ -24,13 +24,13 @@ function sortColor() {
   }
 }
 
-let count = document.querySelector('#span-score');
+let count = document.querySelector('#score');
 
 function play() {
   sortColor();
-  let selectedColor = rgbColorTextFunction();
+  const selectedColor = rgbColorTextFunction();
   let x = parseInt(Math.random() * 6, 10);
-  if (x === 6) { x = 5 }
+  if (x === 6) { x = 5 };
   ballColorsContainer.children[x].style.backgroundColor = `rgb${selectedColor}`;
   console.log(x);
 }
@@ -40,9 +40,8 @@ function choice() {
     const yourChoice = event.target;
     if (yourChoice.style.backgroundColor === `rgb${rgbColor.innerText}`) {
       answer.innerText = 'Acertou!';
+      score.innerText = `Pontuação`;
       count.innerText = +count.innerText + 3;
-      console.log(count);
-      score.innerText = `${count} pontos`;
     } else {
       answer.innerText = 'Errou! Tente novamente!';
       score.innerText = 'Quase !!!';
@@ -51,5 +50,5 @@ function choice() {
   });
 }
 
-window.onload = () => {play(); choice()};
+window.onload = () => { play(); choice() };
 resetGame.addEventListener('click', play);
