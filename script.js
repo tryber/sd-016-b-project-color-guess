23650534bf90eac103dad90f7edd11951f5043b7
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-09-19 16:48:01 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const rgbColor = document.getElementById('rgb-color');
 const ball = document.getElementsByClassName('ball');
+const answer = document.getElementById('answer');
+const resetGame = document.getElementById('reset-game');
+const scoreText = document.getElementById('score');
 let color = '';
+let score = '';
+
+answer.innerText = 'Escolha uma cor';
 
 function randomColor() {
   const red = Math.floor(Math.random() * 255);
@@ -10,9 +16,13 @@ function randomColor() {
   return rgb;
 }
 
-for (let index = 0; index < ball.length; index += 1) {
-  ball[index].style.backgroundColor = `rgb${randomColor()}`;
+function generateColors() {
+  for (let index = 0; index < ball.length; index += 1) {
+    ball[index].style.backgroundColor = `rgb${randomColor()}`;
+  }
 }
+
+generateColors();
 
 function getPositionColor() {
   const min = 0;
@@ -21,17 +31,34 @@ function getPositionColor() {
 }
 
 color = ball[getPositionColor()].style.backgroundColor;
-
 rgbColor.innerText = color;
+
+if (sessionStorage === null) {
+  score = sessionStorage.setItem('score', 0);
+} else {
+  score = parseInt(sessionStorage.getItem('score'));
+}
+scoreText.innerText = `Placar: ${score}`;
 
 function selectedColor(event) {
   if (event.target.style.backgroundColor === color) {
-    console.log('Acertou');
+    // console.log('Acertou');
+    score += 3;
+    sessionStorage.setItem('score', score);
+    answer.innerText = 'Acertou!';
   } else {
-    console.log('Errou!');
+    // console.log('Errou!');
+    answer.innerText = 'Errou! Tente novamente!';
   }
 }
 
 for (let index = 0; index < ball.length; index += 1) {
   ball[index].addEventListener('click', selectedColor);
 }
+
+resetGame.addEventListener('click', () => {
+  // sessionStorage.setItem('score', 0);
+  // window.location.reload(true);
+  answer.innerText = 'Escolha uma cor';
+  generateColors();
+});
