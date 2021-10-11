18731b84110ce917e36fe5521d42591de432c6ea
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-11 10:27:43 -0300

    requisito 1 ok

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-console.log('ok');
+function sorteia() {
+  document.querySelector('#rgb-color').innerText = `(${168}, ${34}, ${1})`;
+}
 
+function configurar() {
+  sorteia();
+}
+
+window.onload = configurar;
 // linha em branco pro linter ficar ok
