9bdba1666fff59fdc501e4e4f4cba03e39a7986b
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-11 11:15:53 -0300

    enche de bolas

--- script.js
+++ script.js
@@ -3,15 +3,27 @@ function sorteiaByte() {
 }
 
 function sorteiaCor() {
-  return [sorteiaByte(), sorteiaByte(), sorteiaByte()];
+  return `rgb(${sorteiaByte()}, ${sorteiaByte()}, ${sorteiaByte()}`;
 }
 
 function sorteia() {
   document.querySelector('#rgb-color').innerText = `(${168}, ${34}, ${1})`;
 }
 
+function criaBola() {
+  const bola = document.createElement('div');
+  bola.classList.add('ball');
+  bola.style.backgroundColor = sorteiaCor();
+  document.querySelector('#bolas').appendChild(bola);
+}
+
+function apresentaBolas() {
+  document.querySelector('#bolas').innerHTML = '';
+  for (let i = 0; i < 6; i += 1) criaBola();
+}
+
 function reset() {
-  console.log(sorteiaCor());
+  apresentaBolas();
   document.querySelector('#score').innerText = 0;
   document.querySelector('#answer').innerText = 'Escolha uma Cor';
   sorteia();
