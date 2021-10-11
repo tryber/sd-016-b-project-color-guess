231ac8f4bcda22efdde9bee8e3230cf80f7ae459
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-11 11:03:54 -0300

    criadas funções pra gerar cor RGB

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+function sorteiaByte() {
+  return Math.ceil(255 * Math.random()) - 1;
+}
+
+function sorteiaCor() {
+  return [sorteiaByte(), sorteiaByte(), sorteiaByte()];
+}
+
 function sorteia() {
   document.querySelector('#rgb-color').innerText = `(${168}, ${34}, ${1})`;
 }
 
 function reset() {
+  console.log(sorteiaCor());
+  document.querySelector('#score').innerText = 0;
   document.querySelector('#answer').innerText = 'Escolha uma Cor';
   sorteia();
 }
 
 function configurar() {
+  document.querySelector('#reset-game').addEventListener('click', reset);
   reset();
 }
 
