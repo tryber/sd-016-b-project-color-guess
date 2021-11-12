92d2cc201a9dab7d57855413b51005240decf3eb
Eduardo Fradique <edsfradique@gmail.com>
2021-11-12 00:40:42 -0300

    :memo: requisito 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const guessColor = document.querySelector('#rgb-color');
+const createBall = document.querySelectorAll('.ball');
 
 const randomRGB = () => Math.trunc(Math.random() * 255) + 1;
 
@@ -6,3 +7,7 @@ const makeRGBColor = () =>
   `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
 
 guessColor.textContent = makeRGBColor();
+
+for (let i = 0; i < createBall.length; i += 1) {
+  document.querySelectorAll('.ball')[i].style.backgroundColor = makeRGBColor();
+}
