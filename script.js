f66268057ac9c6827801c223965322699ec734a0
Eduardo Fradique <edsfradique@gmail.com>
2021-11-12 02:16:20 -0300

    :memo: requisito 4 e 5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const guessColor = document.querySelector('#rgb-color');
 const createBall = document.querySelectorAll('.ball');
+const answer = document.querySelector('#answer');
 
 const randomRGB = () => Math.trunc(Math.random() * 255) + 1;
 
@@ -8,6 +9,37 @@ const makeRGBColor = () =>
 
 guessColor.textContent = makeRGBColor();
 
+const arr = [
+  guessColor.textContent,
+  makeRGBColor(),
+  makeRGBColor(),
+  makeRGBColor(),
+  makeRGBColor(),
+  makeRGBColor(),
+];
+
+// https://stackoverflow.com/questions/19351759/javascript-random-number-out-of-5-no-repeat-until-all-have-been-used
+const uniqueRandoms = [];
+const numRandoms = 6;
+function makeArr() {
+  if (!uniqueRandoms.length) {
+    for (let i = 0; i < numRandoms; i += 1) {
+      uniqueRandoms.push(i);
+    }
+  }
+  const index = Math.floor(Math.random() * uniqueRandoms.length);
+  const val = uniqueRandoms[index];
+  uniqueRandoms.splice(index, 1);
+  return val;
+}
+
 for (let i = 0; i < createBall.length; i += 1) {
-  document.querySelectorAll('.ball')[i].style.backgroundColor = makeRGBColor();
+  createBall[i].style.backgroundColor = arr[makeArr()];
+  createBall[i].addEventListener('click', (event) => {
+    if (event.target.style.backgroundColor === guessColor.textContent) {
+      answer.textContent = 'Acertou!';
+    } else {
+      answer.textContent = 'Errou! Tente novamente!';
+    }
+  });
 }
